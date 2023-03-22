18c84be0425723b0f1636c9121506ee69e5b53e1
somiar <somiarzj@hotmail.com>
2023-03-22 14:00:22 +0800

    init

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -84,7 +84,7 @@ export default {
       showRegistering: false,
       showNotSendTwitter: false,
       connecting: false,
-      authStep: 'select',
+      authStep: 'login',
       generatingKeys: false,
       showPrivateKey: false,
       ethAddress: '',
